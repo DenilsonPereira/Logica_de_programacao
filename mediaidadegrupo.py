d1d44615da878557ec8b7c08010ec06b857aa272
DenilsonPereira <denilsongunner@gmail.com>
2023-02-23 22:35:14 -0300

    Update mediaidadegrupo.py

--- mediaidadegrupo.py
+++ mediaidadegrupo.py
@@ -5,5 +5,8 @@ i3 = 15
 i4 = 17
 i5 = 16
 
-media = (i1+i2+i3+i4)/4
-print(media)
+mediaA = (i1+i2+i3+i4)/4
+mediaP = (i1+i2+i3+i4+i5)/5
+variacao = ((mediaP/mediaA)-1)*100
+
+print("A media de idade do grupo é: ", mediaA,". E a variação percentual ao incluir uma pessoa de ", i5, "é de:", variacao)
